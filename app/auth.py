9993cd6a8464db34e17d1f578d9e7c0ba8097d7f
wajidminhas <shanitent667@gmail.com>
2025-03-27 22:47:26 +0500

    access token created

--- app/auth.py
+++ app/auth.py
@@ -1,9 +1,10 @@
+from datetime import datetime, timedelta, timezone
 from typing import Annotated
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from sqlmodel import Session, select
-
+from jose import jwt, JWTError
 from app.db import get_session
 from app.model import User
 
@@ -11,6 +12,10 @@ from app.model import User
 pwd_context = CryptContext(schemes=("bcrypt"))
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+SECRET_KEY = "8d10cb8c6ffb5e5ff4ae476c3395803a5fc221ce7bdbb7337b148ec982c01100"
+ALGORITHM = "HS256"
+EXPIRY_MINUTES = 30
 
 
 def hash_password(password):
@@ -48,3 +53,16 @@ def authenticate_user(session : Annotated[Session, Depends(get_session)],
        if not verify_password(password=password, hashed_password=db_user.password):
            return False
        return db_user
+
+
+# ***********         ********** CREATE ACCESS TOKEN ************          ******************
+
+def create_access_token(data: dict, expiry_time : timedelta | None = None):
+    encode_data = data.copy()
+    if expiry_time:
+        expire = datetime.now(timezone.utc) + expiry_time
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    encode_data.update({"exp":expire})
+    encoded_jwt = jwt.encode(encode_data, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 
 
+from datetime import timedelta
 from typing import Annotated
 from fastapi import Depends, FastAPI, HTTPException
 from contextlib import asynccontextmanager
@@ -7,10 +8,10 @@ from contextlib import asynccontextmanager
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlmodel import Session
 
-from app.auth import authenticate_user
+from app.auth import EXPIRY_MINUTES, authenticate_user, create_access_token
 from app.router.user import user_router
 from app.db import create_db_and_tables, get_session
-from app.model import Todo
+from app.model import Todo, Token
 
 
 @asynccontextmanager
@@ -55,8 +56,14 @@ async def get_todo(todo_id: int, session: Annotated[Session, Depends(get_session
 
 #    ************     ***********     login user      ***********     ***********     **********
 
-@app.post("/token")
+# here create a Token model to returen the access token
+@app.post("/token/", response_model=Token) 
 async def login(form_data: Annotated[OAuth2PasswordRequestForm ,Depends()],
                 session: Annotated[Session, Depends(get_session)]):
     user = authenticate_user(session, form_data.username, form_data.password)
-    return user
+    if not user:
+        raise HTTPException(status_code=401, detail="Incorrect username or password")
+    
+    expire_time = timedelta(minutes=EXPIRY_MINUTES)
+    access_token =create_access_token({"sub": form_data.username}, expire_time)
+    return Token(access_token=access_token, token_type="bearer")
